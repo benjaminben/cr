45c02ba639d251dd3cbd7255a5645848458b6a20
benBen <benmbenjamin@gmail.com>
2017-02-28 16:36:34 -0800

    major tweaks board-wide god need to commit more

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-console.log("hi cailin")
+(function() {
+  var email = document.getElementById("email_entry")
+  var submit = document.getElementById("email_submit")
+  email.addEventListener("input", function(e) {
+    if (email.value.length > 0) {
+      submit.style.display = "block"
+    }
+    else {
+      submit.style.display = "none"
+    }
+  })
+})()
