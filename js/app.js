7cd1d6a5f502e5c10a1f001fb378343bc5cfd207
benBen <benmbenjamin@gmail.com>
2017-03-01 19:45:03 -0800

    slightly more compatible footer

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 (function() {
-  var email = document.getElementById("email_entry")
-  var submit = document.getElementById("email_submit")
+  var email = document.getElementById("mce-EMAIL")
+  var submit = document.getElementById("mc-embedded-subscribe")
   email.addEventListener("input", function(e) {
     if (email.value.length > 0) {
       submit.style.display = "block"
