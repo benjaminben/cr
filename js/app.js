e5d65f977e1db53916b607748aee511b689ec6ca
benBen <benmbenjamin@gmail.com>
2017-03-02 02:27:17 -0800

    convert images; add social links

--- js/app.js
+++ js/app.js
@@ -1,4 +1,19 @@
 (function() {
+  var imgsLoaded = 0
+  var imgs = Array.from(document.getElementsByTagName("img"))
+  var imgProms = imgs.map(function(p,i,a) {
+    var img = document.createElement("img")
+    img.addEventListener("load", function(e) {
+      imgsLoaded++
+      if (imgsLoaded === a.length) {
+        console.log("done")
+        document.body.className = ""
+      }
+    })
+    img.src = p.src
+    return img
+  })
+
   var email = document.getElementById("mce-EMAIL")
   var submit = document.getElementById("mc-embedded-subscribe")
   email.addEventListener("input", function(e) {
